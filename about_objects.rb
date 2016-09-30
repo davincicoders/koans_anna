f203a8495c848a0cf021a65f513a471eddfb18fb
Anna Baer <anna.baer@gmail.com>
2016-09-29 22:01:49 -0600

    Fixed line 5 in about_objects.rb
    [#131335789]

--- about_objects.rb
+++ about_objects.rb
@@ -2,15 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal _BATMAN_, 1.5.is_a?(Object)
+    assert_equal true, 1.is_a?(Object)
+    assert_equal __, 1.5.is_a?(Object)
     assert_equal __, "string".is_a?(Object)
     assert_equal __, nil.is_a?(Object)
     assert_equal __, Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal _ROBIN_, 123.to_s
+    assert_equal __, 123.to_s
     assert_equal __, nil.to_s
   end
 
